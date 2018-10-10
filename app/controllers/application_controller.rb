ce0b351aa5bafb30a56fda717227afcb9753931b
mido149z <minhkk149@gmail.com>
2018-10-10 09:10:54 +0700

    day code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 	def intro
-		render html: "GITHUB chua duoc"
+		render html: "day code chua len dc GITHUB"
 		
 	end
 end
